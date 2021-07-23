fca61948e824ca914f55dbdbe50e238f8244f2da
jedi <amajidsinar@gmail.com>
2021-07-23 10:00:02 +0700

    fix test

--- test/sample-test.js
+++ test/sample-test.js
@@ -11,7 +11,7 @@ describe("NFTMarket", function () {
 
     /* deploy the NFT */
     const NFT = await ethers.getContractFactory("NFT")
-    const nft = await NFT.deploy()
+    const nft = await NFT.deploy(marketAddress)
     await nft.deployed()
     const nftContractAddress = nft.address
     
@@ -21,6 +21,10 @@ describe("NFTMarket", function () {
     const auctionPrice = ethers.utils.parseUnits('1', 'ether')
 
     /* create two tokens */
+    await nft.createToken("https://www.mytokenlocation.com")
+    await nft.createToken("https://www.mytokenlocation2.com")
+    
+    /* put both tokens for sale */
     await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice})
     await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice})
 
@@ -29,19 +33,20 @@ describe("NFTMarket", function () {
     /* execute sale of token to another user */
     await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, { value: auctionPrice})
 
-    /* query for and return the unsold items */
+    // /* query for and return the unsold items */
     items = await market.fetchMarketItems()
     items = await Promise.all(items.map(async i => {
-      const tokenUri = await nft.tokenUri(i.tokenId)
+      const tokenUri = await nft.tokenURI(i.tokenId)
       let item = {
         price: i.price.toString(),
-        tokenId: i.tokenUri.toString(),
+        tokenId: i.tokenId.toString(),
         seller: i.seller,
         owner: i.owner,
         tokenUri
       }
       return item
     }))
+    
     console.log('items: ', items)
   });
 });
